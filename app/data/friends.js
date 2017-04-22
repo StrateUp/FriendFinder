1165131e68c4f8773894f47a4c6b5b165d6c4a32
KStrate <kaylaastl@Kaylas-MBP.attlocal.net>
2017-04-21 20:22:20 -0700

    launch please

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+//our array of friends, contains an array of scores
+var friends = [
+  {
+    name: " ",
+    photo: " ",
+    scores: []
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friends;
