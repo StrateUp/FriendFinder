6cae982dc9d4bd3b34fdb603d8d67572b02d3f9b
KStrate <kaylaastl@Kaylas-MBP.attlocal.net>
2017-04-22 08:25:08 -0700

    arrg

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 //our array of friends, contains an array of scores
 var friends = [
   {
-    name: " ",
-    photo: " ",
-    scores: []
+    name: "Kayla",
+    photo: "https://media.xogrp.com/images/bbcbdbcd-ae1a-44b4-b3d1-4452879f8d33~rt_auto-rs_325.h",
+    scores: [5,5,5,5,5,5,5,5,5,5]
   }
 ];
 
